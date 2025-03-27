dcaf0a9ee544426fcba3e372fe28ed34e7872039
JungMINI <kjmhs501@nver.com>
2025-03-28 00:57:21 +0900

    2주차_미션1_중간커밋

---
+++ 제이엠-김정민/vite-project/src/2ND-MISSION/Misson1.tsx
@@ -0,0 +1,16 @@
+import Todo from '../components/Todo';
+// import TodoBefore from '../components/TodoBefore';
+import './Misson1.css';
+
+
+function Mission1() {
+    return( 
+        <div>
+            <Todo/>
+            {/* <TodoBefore /> */}
+            {/* 컴포넌트 분리 전의 tsx파일 */}
+        </div>
+    );
+}
+
+export default Mission1;
--- 제이엠-김정민/vite-project/src/main.tsx
+++ 제이엠-김정민/vite-project/src/main.tsx
@@ -1,11 +1,37 @@
-// import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot } from "react-dom/client";
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
+import Mission1 from "./2ND-MISSION/Misson1.tsx";
 
 createRoot(document.getElementById('root')!).render(
-  // 계속 새로고침을 하게 하는 태그 (개발 중 중복 렌더링 방지용)
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
+  <>
+  <Mission1/>
+  </>
 )
+
+
+
+
+
+/* ===================================================================================================================*/
+//실습할때 사용한 main.tsx
+// // import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// // import App from './App'  -> 첫 컴포넌트 만들기기
+// // import App2 from './App2' //useState 첫 실습
+// // import App3 from './Chapter02/App3' //useState 상태 변경경
+// // import Pratice from './Chapter02/pratice'
+// import UseContext from './Chapter02/UseContext'
+// //가장 상위의 부모요소에 Provider을 씌워주면 됨.
+// import {CounterProvider } from './context/CounterProvider'
+
+
+// createRoot(document.getElementById('root')!).render(
+//   // 계속 새로고침을 하게 하는 태그 (개발 중 중복 렌더링 방지용)
+//   // <StrictMode>
+//   <CounterProvider>
+//       <UseContext />
+//   </CounterProvider>
+//   // </StrictMode>,
+// )
