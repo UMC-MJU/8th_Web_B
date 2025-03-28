eeaecbbcfb900f375e9a1340c420613616567e73
JungMINI <kjmhs501@nver.com>
2025-03-29 00:17:47 +0900

    2주차_미션2+프로젝트-폴더정리

--- 제이엠-김정민/vite-project/src/App.tsx
+++ 제이엠-김정민/vite-project/src/App.tsx
@@ -1,23 +1,10 @@
-import './App.css'
-// 1. List Component를 Import
-import List from './components/List';
+import React from 'react'
+import DarkMode from './02-week/2ND-MISSION2/DarkMode'
 
-function App() {
-  const nickname = 'jungmin'
-  const name = '김정민'
-  const sweetPotato = '고구마'
-  const array = ['REACT', 'NEXT', 'VUE', 'SVELTE', 'ANGULAR', 'REACT-NATIVE']
+const App = () => {
   return (
     <>
-      <strong className='school'>명지대학교</strong>
-      <p className ='student'>{nickname}/{name}</p>
-      <h1>{`${nickname}은 ${sweetPotato} 튀김을 좋아합니다.`}</h1>
-      <ul>
-        {array.map((yaho,idx)=> (
-          // 2. <li key = {idx}>{yaho}입니다.</li> -> List 컴포넌트를 호출출
-          <List key={idx} tech={yaho}/>
-        ))}
-      </ul>
+      <DarkMode/>
     </>
   )
 }
