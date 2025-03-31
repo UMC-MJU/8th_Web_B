7c4c3d0a2aa69c891e2ccb278cfe58ad727b567b
KimYeeun <nabinabi1110@gmail.com>
2025-03-31 21:22:43 +0900

    다크모드

--- 루이-김예은/mission_w2/src/App.tsx
+++ 루이-김예은/mission_w2/src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Todo from "./components/Todo";
+import { ThemeProvider } from "./context/ThemeContext";
 import { TodoProvider } from "./context/TodoContext";
 
 function App() {
   return (
-    <TodoProvider>
-      <Todo />
-    </TodoProvider>
+    <ThemeProvider>
+      <TodoProvider>
+        <Todo />
+      </TodoProvider>
+    </ThemeProvider>
   );
 }
 
