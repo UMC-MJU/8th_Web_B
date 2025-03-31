eb767e0a309fe87c38632c7135bd6f0dc39313b8
KimYeeun <nabinabi1110@gmail.com>
2025-03-31 20:27:39 +0900

    contextAPI 활용하기

---
+++ 루이-김예은/mission_w2/src/components/Todo.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import { useTodo } from "../context/TodoContext";
+
+const Todo = () => {
+  const { todos, dones, handleComplete, handleDelete } = useTodo();
+
+  return (
+    <div className="todo-container">
+      <h1 className="todo-container__header">RUI TODO</h1>
+      <TodoForm />
+      <div className="render-container">
+        <TodoList
+          title="할 일"
+          todos={todos}
+          buttonLabel="완료"
+          buttonColor="rgb(7, 141, 7)"
+          onClick={handleComplete}
+        />
+        <TodoList
+          title="완료"
+          todos={dones}
+          buttonLabel="삭제"
+          buttonColor="#dc3545"
+          onClick={handleDelete}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
