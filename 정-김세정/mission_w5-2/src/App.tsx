5c0d27096577c6a7d3315720593dab77ad9f2349
jeong <sejeonggim68@gmail.com>
2025-05-04 22:10:51 +0900

    5주차 과제 완료

---
+++ 정-김세정/mission_w5-2/src/App.tsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter, RouteObject, RouterProvider } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage.tsx';
+import NotFoundPage from './pages/NotFoundPage.tsx';
+import LoginPage from './pages/LoginPage.tsx';
+import HomeLayout from './layouts/HomeLayout.tsx';
+import SignupPage from './pages/SignupPage.tsx';
+import MyPage from './pages/MyPage.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
+import ProtectedLayout from './layouts/ProtectedLayout.tsx';
+import GoogleLoginRedirectPage from './pages/GoogleLoginRedirectPage.tsx';
+
+const publicRoutes: RouteObject[] = [
+  {
+    path:'/',
+    element:<HomeLayout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {index: true, element: <HomePage />},
+      {path: "login", element: <LoginPage />},
+      {path: "signup", element: <SignupPage />},
+      {path:"v1/auth/google/callback", element: <GoogleLoginRedirectPage />},
+    ],
+  },
+];
+
+const protectedRoutes: RouteObject[] = [
+  {
+    path:"/",
+    element:<ProtectedLayout />,
+    errorElement:<NotFoundPage />,
+    children: [
+      {
+        path:"my",
+        element:<MyPage />,
+      },
+    ],
+  },
+];
+const router = createBrowserRouter([...publicRoutes, ...protectedRoutes]);
+
+function App() {
+  return(
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  );
+}
+
+export default App;
