b2e5b0633f87a99bda7b5ee05717305a98d00b91
KimYeeun <nabinabi1110@gmail.com>
2025-06-01 22:15:24 +0900

    throttling 구현위해 ThrottlePage 및 useThrottle hook 만듦.
    ThrottlePage에는 단순 스크롤(scrollY)만 나타나게 함.

--- 루이-김예은/mission_w4/src/App.tsx
+++ 루이-김예은/mission_w4/src/App.tsx
@@ -12,6 +12,7 @@ import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import LpDetailPage from "./pages/LpDetailPage";
+import ThrottlePage from "./pages/throttlePage";
 
 // publicRoutes: 인증 없이 접근 가능한 라우트
 const publicRoutes: RouteObject[] = [
@@ -25,6 +26,7 @@ const publicRoutes: RouteObject[] = [
       { path: "signup", element: <SignUpPage /> },
       { path: "v1/auth/google/callback", element: <GoogleLoginRedirectPage /> },
       { path: "lps/:lpId", element: <LpDetailPage /> },
+      { path: "/throttle", element: <ThrottlePage /> },
     ],
   },
 ];
